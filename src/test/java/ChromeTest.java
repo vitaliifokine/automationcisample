a6e8061009bd7e53d9553d102df9152602d366c3
Vitalii Fokin <vitaliiifokine@gmail.com>
2020-11-15 22:04:43 -0500

    Initial commit

---
+++ src/test/java/ChromeTest.java
@@ -0,0 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Allure;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ChromeTest {
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+       driver.get("https://mvnrepository.com/");
+        Allure.attachment("Sample", ".png");
+    }
+}
