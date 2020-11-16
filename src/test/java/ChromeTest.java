f6e1ca12e80fa951a1ed37f3dd94f07b24e172c7
Vitalii Fokin <vitaliiifokine@gmail.com>
2020-11-15 22:54:32 -0500

    Sample

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -1,11 +1,17 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class ChromeTest {
     private WebDriver driver;
@@ -28,8 +34,14 @@ public class ChromeTest {
     }
 
     @Test
-    public void test() {
+    public void test() throws IOException {
        driver.get("https://mvnrepository.com/");
-        Allure.attachment("Sample", ".png");
+       captureScreenshot();
+    }
+
+    @Step("Make a screenshot")
+    @Attachment
+    public byte[] captureScreenshot()throws IOException {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
 }
